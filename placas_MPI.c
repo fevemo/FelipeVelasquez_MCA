4f6f48ba01637edd65f1678bd8b00e57c3814b3c
Felipe <f.velasquez10@uniandes.edu.co>
2017-02-07 17:54:10 -0500

    matriz inicializada

--- placas_MPI.c
+++ placas_MPI.c
@@ -13,12 +13,16 @@ int main(){
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  float L = 5, l = 2, d = 1, h = 0.02, V0 = 100, N = 2*pow((L/h), 2);
+  float L = 5, l = 2, d = 1, h = 0.02, V0 = 100, N = 64;
   int n = (int)(L/h);
 
   
   //inicializa la matriz
   int i, j, k;
+  int z=n/world_size;
+  int first_col=z*rank-1;
+  int primera_placa=(int)((L/2-d/2)/h)-first_col;
+  int segunda_placa=(int)((L/2+d/2)/h)-first_col;
   if(rank==0)
 	{
 	double **matriz;
@@ -40,7 +44,7 @@ int main(){
   	}
 	
 	//inicializa las fronteras horizontales
-  	for(j=0; j<=m; j++){
+  	for(j=0; j<m; j++){
     		matriz[0][j] = 0;
     		matriz[n-1][j] = 0;
  	 }
@@ -59,39 +63,117 @@ int main(){
   		}
 	}
 
+	printf("procesador # %d",rank);
+	  for(i=0; i<n; i++){
+    		for(j=0; j<m; j++){
+      			printf("%f |", matriz[i][j]);
+    		}
+		printf("\n");
+  		}
+
 
 	}
+  else if(rank==world_size-1)
+    {
+      
+      double **matriz;
+  	matriz = (double**) malloc(n*sizeof(double*));
+	
+	int m=(n/world_size+1);
+  	for (i=0; i<=n; i++){
+    	matriz[i] = (double*) malloc(m*sizeof(double));
+  	}
+	
+	for(i=0; i<n; i++){
+   		 for(j=0; j<m; j++){
+      			matriz[i][j] = 5;
+    		}
+	}
 
+	//inicializa las fronteras horizontales
+  	for(j=0; j<m; j++){
+    		matriz[0][j] = 0;
+    		matriz[n-1][j] = 0;
+ 	 }
 
-  double **matriz;
-  matriz = (double**) malloc(n*sizeof(double*));
+	//inicializa las fronteras verticales
+  	for(i=0; i<n; i++){
+    		matriz[i][m-1] = 0;
+  	}
 
-  for (i=0; i<=n; i++){
-    matriz[i] = (double*) malloc(n*sizeof(double));
-  }
+	//incluye primera placa
+	if(primera_placa>=0)
+	{
+		for(i=(int)((L/2-l/2)/h); i<(int)((L/2+l/2)/h); i++){
+   			matriz[i][primera_placa] = -V0/2;
+  		}
+	}
 
-  for(i=0; i<n; i++){
-    for(j=0; j<n; j++){
-      matriz[i][j] = 5;
+	//incluye segunda placa
+	if(segunda_placa>=0)
+	{
+		for(i=(int)((L/2-l/2)/h); i<(int)((L/2+l/2)/h); i++){
+   			matriz[i][segunda_placa] = -V0/2;
+  		}
+	}
+	printf("procesador # %d",rank);
+	  for(i=0; i<n; i++){
+    		for(j=0; j<m; j++){
+      			printf("%f ", matriz[i][j]);
+    		}
+		printf("\n");
+  		}
+
     }
-  }
-  //inicializa las placas
-  for(i=(int)((L/2-l/2)/h); i<(int)((L/2+l/2)/h); i++){
-    matriz[i][(int)((L/2-d/2)/h)] = -V0/2;
-    matriz[i][(int)((L/2+d/2)/h)] = V0/2;
-  }
-  //inicializa las fronteras verticales
-  for(i=0; i<n; i++){
-    matriz[i][0] = 0;
-    matriz[i][n-1] = 0;
-  }
-  //inicializa las fronteras horizontales
-  for(j=0; j<=n; j++){
-    matriz[0][j] = 0;
-    matriz[n-1][j] = 0;
-  }
+    else
+    {
+ 
+ 	double **matriz;
+  	matriz = (double**) malloc(n*sizeof(double*));
+	
+	int m=(n/world_size+2);
+  	for (i=0; i<=n; i++){
+    	matriz[i] = (double*) malloc(m*sizeof(double));
+  	}
+	
+	for(i=0; i<n; i++){
+   		 for(j=0; j<m; j++){
+      			matriz[i][j] = 5;
+    		}
+	}
 
+	//inicializa las fronteras horizontales
+  	for(j=0; j<m; j++){
+    		matriz[0][j] = 0;
+    		matriz[n-1][j] = 0;
+ 	 }
 
+	//incluye primera placa
+	if(primera_placa>=0 && primera_placa<m)
+	{
+		for(i=(int)((L/2-l/2)/h); i<(int)((L/2+l/2)/h); i++){
+   			matriz[i][primera_placa] = -V0/2;
+  		}
+	}
+
+	//incluye segunda placa
+	if(segunda_placa>=0 && segunda_placa<m)
+	{
+		for(i=(int)((L/2-l/2)/h); i<(int)((L/2+l/2)/h); i++){
+   			matriz[i][segunda_placa] = -V0/2;
+  		}
+	}
+	printf("procesador # %d",rank);
+	  for(i=0; i<n; i++){
+    		for(j=0; j<m; j++){
+      			printf("%f ", matriz[i][j]);
+    		}
+		printf("\n");
+  		}
+
+    }
+
+  /*
   //inicializa la matriz temporal
   double **matriz2;
   matriz2 = (double**) malloc(n*sizeof(double*));
@@ -171,7 +253,10 @@ int main(){
     }
   }
 
+*/
 
-  return 0;
+ 
+ MPI_Finalize();
+ return 0;
+} 
 
-}
