898af30417815aa4722e8c461f69f8e921e77856
Felipe Velasquez <f.velasquez10@uniandes.edu.co>
2017-04-24 19:50:17 -0500

    base para support vector machine

---
+++ SVM.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn import datasets
+from sklearn.datasets import make_classification
+#from sklearn.model_selection import train_test_split
+
+data=datasets.load_breast_cancer()
+data.frame()
+
+#X_train, X_test, Y_train, Y_test=train train_test_split()
+
+C=1.0
+gamma=1.0
+kernel_svc=svm.SVC(C=C,kernel='rbf',gamma=gamma)
+kernel_svc.fit
